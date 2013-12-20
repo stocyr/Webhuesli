5b3aa34a7f67629f5eed1a5e66f4bdc96839cccb
Cyril Stoller <cyril.stoller@gmail.com>
2013-12-20 14:24:04 +0100

    complete refactoring of practically all variable names and callback routines, fixes #1

--- Website/Website/javascript/canvas.js
+++ Website/Website/javascript/canvas.js
@@ -5,11 +5,11 @@ var ctx=canvas.getContext("2d");
 /* Andere globale Variablen */
 var consoleLog = document.getElementById("consoleLog");
 var webSocket;
-var stellwertHeizung = 100;
-var kl = document.getElementById("s_kronleuchter");
-var lp = document.getElementById("s_lampe");
+var heizung_ist_defaultwert = 100;
+var kronleuchter = document.getElementById("slider_kronleuchter");
+var lampe = document.getElementById("slider_lampe");
 var tv = document.getElementById("label_tv");
-var hz = document.getElementById("s_heizung");
+var heizung_soll = document.getElementById("slider_heizung_soll");
 
 //var rangeslider = document.getElementByID("rangevalue");
 
@@ -26,20 +26,20 @@ function on_canvas_click(ev) {
   ctx.clearRect(0,300,400,50);  /* alten Text loeschen */
   ctx.fillStyle = "#000000";
   ctx.fillText(message,10,315);  /* neuen Text schreiben */
-  message = "Kronleuchter: " +kronleuchter.value + " Lampe : " + lampe.value + " Heizung Soll: " +heizung.value +" Ist: " + heizung_ist.value;
+  message = "Kronleuchter: " +label_kronleuchter.value + " Lampe : " + label_lampe.value + " Heizung Soll: " +label_heizung_soll.value +" Ist: " + label_temp_ist.value;
   ctx.fillText(message, 10, 330);
   
   
   /* wenn auf Kronleuchter gedrückt */
   if(x > 196 && x < (196 + 22) && y > 156 && y < (156 + 22)){
 		
-		if(kl.value < 1){
-			kl.value = 100;
+		if(kronleuchter.value < 1){
+			kronleuchter.value = 100;
 		}
 		else{
-			kl.value = 0;
+			kronleuchter.value = 0;
 		}
-		kl.onchange();
+		kronleuchter.onchange();
         
         logToConsole('<span style="color: blue;">Kronleuchter: ' + kronleuchter.value+'</span>');
 	}
@@ -47,13 +47,13 @@ function on_canvas_click(ev) {
 	/* wenn auf Lampe gedrückt */
   if(x > 248 && x < (248 + 21) && y > 196 && y < (196 + 17)){
 		
-		if(lp.value < 1){
-			lp.value = 100;
+		if(lampe.value < 1){
+			lampe.value = 100;
 		}
 		else{
-			lp.value = 0;
+			lampe.value = 0;
 		}
-		lp.onchange();
+		lampe.onchange();
         
         logToConsole('<span style="color: blue;">Lampe: ' + lampe.value+'</span>');
 	}
@@ -66,13 +66,13 @@ function on_canvas_click(ev) {
         else{
             tv.value = "OFF";
         }
-        tv_change(tv.value);
+        tv_change_callback(tv.value);
 		
 		logToConsole('<span style="color: blue;">TV: ' + tv.value+'</span>');
 	}
 }
 
-function tv_change(value){
+function tv_change_value(value){
     /* Bild zeichnen */
     var tvbild = new Image();
     
@@ -86,12 +86,15 @@ function tv_change(value){
     tvbild.onload = function() {
         ctx.drawImage(tvbild, 163, 227, 23, 14);
     };
-    /* Hier Wert an BeagleBone senden */
-    sendTvPair(value);
 }
 
-/* Handler für Ständerlampe, GUI aktualisieren und Kommunikation zum Huesli */
- function lamp_change(slideAmount){
+function tv_change_callback(value){
+    tv_change_value();
+    /* Hier Wert an BeagleBone senden */
+    send_tv_pair(value);
+}
+
+function lampe_change_value(slideAmount){
 	/* Bild zeichnen */
 	var img_lampe_on = new Image();
 	var img_lampe = new Image();
@@ -108,13 +111,17 @@ function tv_change(value){
 		ctx.drawImage(img_lampe_on, 248, 195, 21, 17);
 		ctx.globalAlpha = 1.0;
 	};
+}
+
+/* Handler für Ständerlampe, GUI aktualisieren und Kommunikation zum Huesli */
+ function lampe_slider_callback(slideAmount){
+    lampe_change_value();
 	/* Hier Wert an BeagleBone senden */
-    sendLampPair(slideAmount);
+    send_lamp_pair(slideAmount);
  }
  
- /* Handler für Kronleuchter, GUI aktualisieren und Kommunikation zum Huesli */
- function kron_change(slideAmount){
-	/* Bild zeichnen */
+ function kronleuchter_change_value(slideAmount){
+ 	/* Bild zeichnen */
 	var img_lampe_on = new Image();
 	var img_lampe = new Image();
 	
@@ -130,21 +137,25 @@ function tv_change(value){
 		ctx.drawImage(img_lampe_on, 196, 156, 22, 22);
 		ctx.globalAlpha = 1.0;
 	};
-	/* Hier Wert an BeagleBone senden */
-	sendLeuchterPair(slideAmount);
-    sendTvPair("OFF");
  }
  
- function heizung_soll_change(slideAmount){
+ /* Handler für Kronleuchter, GUI aktualisieren und Kommunikation zum Huesli */
+ function kronleuchter_slider_callback(slideAmount){
+    kronleuchter_change_value();
+	/* Hier Wert an BeagleBone senden */
+	send_kronleuchter_pair(slideAmount);
+ }
+ 
+ function heizung_soll_slider_callback(slideAmount){
 	/* Hier Wert an BeagleBone senden */
 	sendHeizungPair(slideAmount);
  }
 
-/* zu Teszwecken hier heizung setzen */
-heating(stellwertHeizung);
+/* zu Teszwecken hier heizung_soll setzen */
+heizung_ist_change(heizung_ist_defaultwert);
 
 /* Illustrates the Heating control value */
- function heating(stellwert){
+ function heizung_ist_change(stellwert){
 	/* Bild rechts zeichnen */
 	var img_heating_on = new Image();
 	var img_heating = new Image();
@@ -177,7 +188,6 @@ heating(stellwertHeizung);
 		ctx.drawImage(img_heating_on_left, 132, 237, 9, 30);
 		ctx.globalAlpha = 1.0;
 	};
-	
  }
  
  /* Handler fure Mouse-click registrieren */ 
@@ -232,48 +242,47 @@ function onMessage(evt)
     
     /* Es könnten mehrere Pairs in einer JSON Message sein, also damit rechnen */
     var jsonObject = JSON.parse(evt.data);
-    /*for(var key in jsonObject){
-        ...
-    }*/
+
+    // Special Server INIT values:
     if(jsonObject.TV){
         tv.value = jsonObject.TV;
-        tv_change(tv.value);
+        tv_change_value(tv.value);
     }
     if(jsonObject.Lampe){
-        lp.value = jsonObject.Lampe;
-        lamp_change(jsonObject.Lamp);
+        lampe.value = jsonObject.Lampe;
+        lampe_change_value(jsonObject.Lamp);
     }
     if(jsonObject.Leuchter){
-        kl.value = jsonObject.Leuchter;
-        kron_change(jsonObject.Leuchter);
+        kronleuchter.value = jsonObject.Leuchter;
+        kronleuchter_change_value(jsonObject.Leuchter);
     }
     if(jsonObject.TempSoll){
-        hz.value = jsonObject.TempSoll;
-        heizung_soll_change(jsonObject.TempSoll);
+        heizung_soll.value = jsonObject.TempSoll;
+        heizung_soll_change_value(jsonObject.TempSoll);
     }
+    
+    // Normal Server INFO values:
     if(jsonObject.TempIst){
-        hz.value = jsonObject.TempSoll;
-        heizung_soll_change(jsonObject.TempSoll);
+        label_temp_ist.value = jsonObject.TempSoll;
     }
     if(jsonObject.Heizung){
-        stellwertHeizung = jsonObject.TempSoll;
-        heating(jsonObject.TempSoll);
+        heizung_ist_change(jsonObject.TempSoll);
     }  
 }
 
-function sendTvPair(val){
+function send_tv_pair(val){
     var sendObj = { TV: val };
     webSocket.send(JSON.stringify(sendObj));
     logToConsole("SENT: " + JSON.stringify(sendObj));
 }
 
-function sendLampPair(val){
+function send_lamp_pair(val){
     var sendObj = { Lampe: val };
     webSocket.send(JSON.stringify(sendObj));
     logToConsole("SENT: " + JSON.stringify(sendObj));
 }
 
-function sendLeuchterPair(val){
+function send_kronleuchter_pair(val){
     var sendObj = { Leuchter: val };
     webSocket.send(JSON.stringify(sendObj));
     logToConsole("SENT: " + JSON.stringify(sendObj));
